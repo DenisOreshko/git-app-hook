41e7261c8493576a7baf16649ca08569a9ae489c
Denis <denis7371@gmail.com>
2022-05-18 17:15:49 +0200

    delete comment cod

--- src/components/error403Message/error403Message.js
+++ src/components/error403Message/error403Message.js
@@ -3,40 +3,6 @@ import GitHubService from '../../services/GitHubService';
 import img403 from './403.png';
 import './error403Message.css';
 
-// class ErrorMessage extends  Component{
-
-//     state = { 
-//         reset: 0
-//     }
-
-//     gitHubService = new GitHubService();
-
-//     onRateLoaded = (reset) => {
-//         this.setState({reset})
-//     }
-
-//     componentDidMount(){
-//         this.gitHubService
-//             .getRateLimitRemaining()
-//             .then(this.onRateLoaded)    
-//     }
-
-//     render(){
-//         let hour = new Date(+this.state.reset * 1000).getHours();
-//         let minute = new Date(+this.state.reset * 1000).getMinutes();
-//         let second = new Date(+this.state.reset * 1000).getSeconds();
-        
-//         return(
-//             <div className="error">
-//                 <img src={img403} alt="Error"/>
-//                 <span>GitHub API request limit exceeded</span> 
-//                 <span>Please try again in {hour}:{minute}:{second}</span>               
-//             </div>
-            
-//         )
-//     }    
-// }
-
 const Error403Message = (prop) => {
 
     const [ret, setRet] = useState(0);
