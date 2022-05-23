f82a88796467c1fdb076a0e0dd6e552fe89218f5
Denis <denisoreshkoofficial@gmail.com>
2022-05-23 22:19:41 +0300

    del ServerError.js

--- src/services/ServerError.js
+++
@@ -1,9 +0,0 @@
-class ServerError extends Error{
-    constructor(message, status){
-        super(message);
-        this.status = status;
-        this.name = 'ServerError';
-    }
-}
-
-export default ServerError;
