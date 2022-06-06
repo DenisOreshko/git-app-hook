1989d0c654829f01c77b5ef7427e74be32b411a3
Denis <denis7371@gmail.com>
2022-06-06 17:34:00 +0200

    add transition routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {useEffect, useState, lazy, Suspense} from 'react';
-import {Route, Routes, useNavigate} from 'react-router-dom';
+import {Route, Routes, useNavigate, useLocation} from 'react-router-dom';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
 import './App.css';
 import Header from './components/header/header.js';
 import InitialPage from './components/initialPage/initialPage';
 import useGitHubService from './services/GitHubService';
 import ContentLoadPage from './components/contentLoadPage/contentLoadPage.js';
-import { LoginContext } from './context';
+import {LoginContext} from './context';
 const UserNotFoundPage = lazy(()=> import('./components/userNotFoundPage/userNotFoundPage'));
 
 const App = () => {
@@ -16,31 +17,39 @@ const App = () => {
       console.log(`onSearchUserApp() error ${error}`);      
     } 
     clearError();    
-    //localStorage.setItem('path', search);
     (search === '') ? navigate(`/`) : navigate(`/users/${search}`);  
   }
 
   const [loginContext, setLoginContext] = useState('');
 
+  // const routes = [
+  //   { path: '/', name: 'InitPage', Component: InitialPage },
+  //   { path: '/users/:login', name: 'ContentLoadPage', Component: ContentLoadPage },
+  //   { path: '*', name: 'UserNotFoundPage', Component: UserNotFoundPage },
+  // ]
+  const location = useLocation();
   return (  
-      <LoginContext.Provider value={{
-        loginContext,
-        setLoginContext
-      }}>
-        <div className="App">
-              <Header onSearchUserApp={onSearchUserApp}/>
-              <div>
-                <Suspense fullback={<span>Loading...</span>}>
-                    <Routes> 
-                      <Route exact path="/" element={<InitialPage/>}/>
-                      <Route exact path="/users/:login" element={<ContentLoadPage/>}/>           
-                      <Route path="*" element={<UserNotFoundPage/>}/>                  
-                    </Routes>
-                </Suspense>                
-              </div>
-        </div>
-      </LoginContext.Provider>
+    <LoginContext.Provider value={{
+      loginContext,
+      setLoginContext
+    }}>
+      <div className="App">
+            <Header onSearchUserApp={onSearchUserApp}/>
+            <div>
+               <Suspense fullback={<span>Loading...</span>}>
+                  <TransitionGroup component={null}>
+                    <CSSTransition key={location.key} timeout={300} classNames="fade">
+                      <Routes location={location}>
+                          <Route exact path={'/'} element={<InitialPage/>}/> 
+                          <Route exact path={'/users/:login'} element={<ContentLoadPage/>}/> 
+                          <Route exact path={'*'} element={<UserNotFoundPage/>}/> 
+                      </Routes>
+                    </CSSTransition>
+                  </TransitionGroup>
+              </Suspense>                
+            </div>
+      </div>
+    </LoginContext.Provider>
   )
 }
-
 export default App;
